4233b2c6cdf9d264c0cf66a03ef0d27eeeb870c8
DJ <dtaylor88@gmail.com>
2021-11-23 17:16:13 -0800

    eslint finish up actions folder

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -1,12 +1,12 @@
 export const fetchProducts = () => {
-    return (dispatch) => {
-        dispatch({ type: 'LOADING_PRODUCTS' });
-        fetch('https://bazaar-react-api.herokuapp.com/api/v1/products')
-        .then((response) => {
-            return response.json();
-        })
-        .then((responseJSON) => {
-            dispatch({ type: 'ADD_PRODUCTS', products: responseJSON })
-        })
-    }
+  return (dispatch) => {
+    dispatch({ type: 'LOADING_PRODUCTS' });
+    fetch('https://bazaar-react-api.herokuapp.com/api/v1/products')
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseJSON) => {
+        dispatch({ type: 'ADD_PRODUCTS', products: responseJSON })
+      })
+  }
 }
--- src/actions/sessionActions.js
+++ src/actions/sessionActions.js
@@ -1,31 +1,31 @@
 export const googleLogin = (response) => {
-    return (dispatch) => {
-        dispatch({ type: 'LOADING_USER' });
+  return (dispatch) => {
+    dispatch({ type: 'LOADING_USER' });
     const token = response.tokenId
     const requestOptions = {
-        method: 'POST',
-        headers: {
-            'Authorization': `Bearer ${response.accessToken}`,
-            'Content-Type': 'application/json',
-            'access-token': `${response.accessToken}`
-        },
-        body: JSON.stringify(token)
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${response.accessToken}`,
+        'Content-Type': 'application/json',
+        'access-token': `${response.accessToken}`
+      },
+      body: JSON.stringify(token)
     }
     return fetch('https://bazaar-react-api.herokuapp.com/api/v1/auth/request', requestOptions)
-    .then(response => response.json())
-    .then((responseJSON) => {
+      .then(response => response.json())
+      .then((responseJSON) => {
         if (!responseJSON.status) {
-        dispatch({ type: 'LOGIN', user: responseJSON });
-        localStorage.setItem('user', JSON.stringify(responseJSON));
+          dispatch({ type: 'LOGIN', user: responseJSON });
+          localStorage.setItem('user', JSON.stringify(responseJSON));
         } else {
-            dispatch({ type: 'LOGIN_ERROR', status: 'error' })
+          dispatch({ type: 'LOGIN_ERROR', status: 'error' })
         }
-    })
-    }
+      })
+  }
 }
 
 export const logout = () => {
-    return {
-        type: 'LOGOUT',
-    };
+  return {
+    type: 'LOGOUT',
+  };
 }
