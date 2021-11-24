69c56192263206e4a727950871fb92698d951ff8
DJ <dtaylor88@gmail.com>
2021-11-23 17:30:40 -0800

    eslint cleanup welcome and navbar

--- src/components/home/Welcome.js
+++ src/components/home/Welcome.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 import { Typography } from '@material-ui/core';
 
 const Welcome = () => {
-    return (
-        <div style={{ display: 'flex', flexDirection: 'column', justifyContent:'center', alignItems: 'center', marginTop: '5rem' }}>
-        <Typography variant='h5'>your local marketplace</Typography>
-        <Link to='/products' style={{ textDecoration: 'none'}}>shop</Link> 
-       </div>
-    );
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', justifyContent:'center', alignItems: 'center', marginTop: '5rem' }}>
+      <Typography variant='h5'>your local marketplace</Typography>
+      <Link to='/products' style={{ textDecoration: 'none'}}>shop</Link> 
+    </div>
+  );
 };
 
 export default Welcome;
--- src/components/navigation/NavBar.js
+++ src/components/navigation/NavBar.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
-import { AppBar, Toolbar, IconButton, Typography } from "@material-ui/core";
-import { connect } from "react-redux";
+import { AppBar, Toolbar, IconButton, Typography } from '@material-ui/core';
+import { connect } from 'react-redux';
 import CartDrawer from '../cart/CartDrawer';
-import { googleLogin, logout } from "../../actions/sessionActions";
+import { googleLogin, logout } from '../../actions/sessionActions';
 import SortIcon from '@mui/icons-material/Sort';
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
@@ -13,19 +13,19 @@ import Logout from '../users/Logout'
 import Avatar from '@mui/material/Avatar';
 
 const useStyles = makeStyles(() => ({
-    appbar: {
-        background: 'none'
-    },
-    icon: {
-        color: '#fff'
-    },
+  appbar: {
+    background: 'none'
+  },
+  icon: {
+    color: '#fff'
+  },
 }));
 
 const  NavBar = (props) => {
-    let history = useHistory();
-    const { user } = props.user;
+  let history = useHistory();
+  const { user } = props.user;
 
-const [anchorEl, setAnchorEl] = React.useState(null);
+  const [anchorEl, setAnchorEl] = React.useState(null);
 
 
   const handleMenu = (event) => {
@@ -42,8 +42,8 @@ const [anchorEl, setAnchorEl] = React.useState(null);
   }
 
   const handleHome = () => {
-      history.push('/')
-      setAnchorEl(null);
+    history.push('/')
+    setAnchorEl(null);
   }
 
   const handleShop = () => {
@@ -52,67 +52,66 @@ const [anchorEl, setAnchorEl] = React.useState(null);
   }
 
   const handleLogout = () => {
-      setAnchorEl(null)
+    setAnchorEl(null)
   }
 
   const classes = useStyles();
-    return (
-      <AppBar elevation={0} className={classes.appbar} position="static" style={{ display: "flex" }}>
-        <Toolbar>
-          <Typography variant="h5" onClick={handleHome} style={{ color: 'black', flexGrow: '1', cursor: 'pointer' }}>Bazaar</Typography>
-            <div>
-               <IconButton
-                size="medium"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
-              >
-                <SortIcon className={classes.icon} />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-              >
+  return (
+    <AppBar elevation={0} className={classes.appbar} position='static' style={{ display: 'flex' }}>
+      <Toolbar>
+        <Typography variant='h5' onClick={handleHome} style={{ color: 'black', flexGrow: '1', cursor: 'pointer' }}>Bazaar</Typography>
+        <div>
+          <IconButton
+            size='medium'
+            aria-controls='menu-appbar'
+            aria-haspopup='true'
+            onClick={handleMenu}
+          >
+            <SortIcon className={classes.icon} />
+          </IconButton>
+          <Menu
+            id='menu-appbar'
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
                 
-                <MenuItem onClick={handleHome}>Home</MenuItem>
-                <MenuItem onClick={handleShop}>Shop</MenuItem>
+            <MenuItem onClick={handleHome}>Home</MenuItem>
+            <MenuItem onClick={handleShop}>Shop</MenuItem>
                 
-                { props.user.isAuth && (
-                <div>                  
+            { props.user.isAuth && (
+              <div>                  
                 <MenuItem onClick={handleOrders}>My orders</MenuItem>
                 <MenuItem onClick={handleLogout}><Logout logout={props.logout}/></MenuItem>
-                </div> 
-                 )}
-                </Menu>
-            </div>
-            {props.user.isAuth ? (
-            <>
+              </div> 
+            )}
+          </Menu>
+        </div>
+        {props.user.isAuth ? (
+          <>
             <CartDrawer className={classes.cart} cart={props.cart} products={props.products} removeFromCart={props.removeFromCart} addToCart={props.addToCart}/>
             <Avatar src={`${user.image}`} />
-            </>
-            ) : (
-            
-            <>
+          </>
+        ) : ( 
+          <>
             <div style={{ cursor: 'pointer' }}>
-            <Login login={props.googleLogin} />
+              <Login login={props.googleLogin} />
             </div>
-            </>
-            )}
-        </Toolbar>
-      </AppBar>
-    );
-  }
+          </>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+}
 
 export default connect(({ user }) => ({ user }), { logout, googleLogin })(
   NavBar
