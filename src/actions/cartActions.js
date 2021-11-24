dcb4ff893d5c000c1cfa8b617b95020aadae16b9
DJ <dtaylor88@gmail.com>
2021-11-23 17:11:05 -0800

    eslint clean up of cartActions file

--- src/actions/cartActions.js
+++ src/actions/cartActions.js
@@ -1,51 +1,51 @@
 export const addToCart = (id) => {
-    return (dispatch) => {
-        dispatch({ type: 'LOADING_CART' });
+  return (dispatch) => {
+    dispatch({ type: 'LOADING_CART' });
     const user = JSON.parse(localStorage.getItem('user'));
 
     const requestOptions = {
-        method: 'PATCH',
-        headers: {
-            'Authorization': `Bearer ${user.google_token}`,
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-            'access-token': `${user.google_token}`
-        },
-        body: JSON.stringify({user_id: user.id, product_id: id })
+      method: 'PATCH',
+      headers: {
+        'Authorization': `Bearer ${user.google_token}`,
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'access-token': `${user.google_token}`
+      },
+      body: JSON.stringify({user_id: user.id, product_id: id })
     }
     return fetch(`https://bazaar-react-api.herokuapp.com/api/v1/carts/${user.cart.id}`, requestOptions)
-    .then(response => response.json())
-    .then((responseJSON) => {
+      .then(response => response.json())
+      .then((responseJSON) => {
         dispatch({ type: 'ADD_TO_CART', product: responseJSON })
-    })
-    }
+      })
+  }
 }
 
 export const removeProductFromCart = (id) => {
-    return (dispatch) => {
-        dispatch({ type: 'LOADING_CART' });
+  return (dispatch) => {
+    dispatch({ type: 'LOADING_CART' });
     const user = JSON.parse(localStorage.getItem('user'));
 
     const requestOptions = {
-        method: 'PATCH',
-        headers: {
-            'Authorization': `Bearer ${user.google_token}`,
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-            'access-token': `${user.google_token}`
-        },
-        body: JSON.stringify({user_id: user.id, product_id: id})
+      method: 'PATCH',
+      headers: {
+        'Authorization': `Bearer ${user.google_token}`,
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'access-token': `${user.google_token}`
+      },
+      body: JSON.stringify({user_id: user.id, product_id: id})
     }
     return fetch(`https://bazaar-react-api.herokuapp.com/api/v1/carts/${user.cart.id}/remove`, requestOptions)
-    .then(response => response.json())
-    .then((responseJSON) => {
+      .then(response => response.json())
+      .then((responseJSON) => {
         dispatch({ type: 'REMOVE_FROM_CART', product: responseJSON })
-    })
-    }
+      })
+  }
 }
 
 export const clearCart = () => {
-    return {
-        type: 'CLEAR_CART'
-    };
+  return {
+    type: 'CLEAR_CART'
+  };
 }
