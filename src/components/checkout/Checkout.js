f9e75204e0aab2b102e871a14958dd8deb621c17
DJ <dtaylor88@gmail.com>
2021-11-23 17:22:51 -0800

    eslint cleanup checkout.js

--- src/components/checkout/Checkout.js
+++ src/components/checkout/Checkout.js
@@ -10,97 +10,92 @@ import Loading from '../notifications/Loading'
 
 
 const Checkout = (props) => {
-    let history = useHistory();
-    const { cart } = props
-    const [address, setAddress] = useState('');
-    const [city, setCity] = useState('');
-    const [state, setState] = useState('');
-    const [loading, setLoading] = useState(false)
+  let history = useHistory();
+  const { cart } = props
+  const [address, setAddress] = useState('');
+  const [city, setCity] = useState('');
+  const [state, setState] = useState('');
+  const [loading, setLoading] = useState(false)
 
-    const elements = useElements();
-    const stripe = useStripe();
+  const elements = useElements();
+  const stripe = useStripe();
 
-    const user = JSON.parse(localStorage.getItem('user'));
+  const user = JSON.parse(localStorage.getItem('user'));
 
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        setLoading(true);
-        if (!stripe || !elements) {
-            return;
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const {clientSecret, order} = await fetch('https://bazaar-react-api.herokuapp.com/api/v1/create-payment-intent', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        paymentMethodType: 'card',
+        amount: `${cart.total * 100}`,
+        currency: 'usd',
+        user: user,
+        address: address,
+        city: city,
+        state: state,
+        order_number: Math.floor(Math.random() * 1000)
+      }),
+    }).then(r => r.json())
+    const {paymentIntent} = await stripe.confirmCardPayment(
+      clientSecret, {
+        payment_method: {
+          card: elements.getElement(CardElement),
         }
+      })
 
-        const {clientSecret, order} = await fetch('https://bazaar-react-api.herokuapp.com/api/v1/create-payment-intent', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            body: JSON.stringify({
-                paymentMethodType: 'card',
-                amount: `${cart.total * 100}`,
-                currency: 'usd',
-                user: user,
-                address: address,
-                city: city,
-                state: state,
-                order_number: Math.floor(Math.random() * 1000)
-            }),
-        }).then(r => r.json())
+    if (paymentIntent){
+      props.addOrder(order)
+      props.clearCart()
+      elements.getElement(CardElement).clear()
+      setAddress('')
+      setCity('')
+      setState('')
+      history.push('/confirmation')
+    }
+  }
 
-        const {paymentIntent} = await stripe.confirmCardPayment(
-            clientSecret, {
-                payment_method: {
-                    card: elements.getElement(CardElement),
-                }
-            })
-
-            if (paymentIntent){
-                props.addOrder(order)
-                props.clearCart()
-                elements.getElement(CardElement).clear()
-                setAddress('')
-                setCity('')
-                setState('')
-                history.push('/confirmation')
-            }
-        }
-
-    return (
-        <div>
-            {loading ? <Loading /> : null}
-          <Grid container spacing={4} alignItems='center' justifyContent='center' style={{ maxWidth: 1100, margin: '0 auto' }}>
-            <Grid item sm={8}>
-              <form id='payment-form' onSubmit={(e) => handleSubmit(e)}>
-                <Typography variant='h5'>Shipping Info</Typography>
-                <br></br>
-                <Grid item xs={10}>
-                <TextField fullWidth variant='standard' label="street address" value={address} onChange={e => setAddress(e.target.value)}></TextField>
-                </Grid>
-                <Grid item xs={10}>
-                <TextField fullWidth label='city' variant='standard' value={city} onChange={e => setCity(e.target.value)}></TextField>
-                </Grid>
-                <Grid item xs={10}>
-                <TextField fullWidth label='state' variant='standard' value={state} onChange={e => setState(e.target.value)}></TextField>
-                </Grid>
-                <br></br>
-                <Typography variant='h5'>Payment</Typography>
-               
-                 <Box sx={{ borderColor: 'grey', borderRadius: 6, height: '2rem', marginBottom: '2rem', marginTop: '.5rem', bgcolor: 'white', padding: '1rem', alignItems: 'center' }}>
-                 <CardElement id="card-element" iconStyle='solid' />
-                 </Box>
-                 <Button variant='contained' type='submit'>Pay</Button>
-             
-                </form>
-
+  return (
+    <div>
+      {loading ? <Loading /> : null}
+      <Grid container spacing={4} alignItems='center' justifyContent='center' style={{ maxWidth: 1100, margin: '0 auto' }}>
+        <Grid item sm={8}>
+          <form id='payment-form' onSubmit={(e) => handleSubmit(e)}>
+            <Typography variant='h5'>Shipping Info</Typography>
+            <br></br>
+            <Grid item xs={10}>
+              <TextField fullWidth variant='standard' label="street address" value={address} onChange={e => setAddress(e.target.value)}></TextField>
             </Grid>
-            <Grid item sm={4} style={{ color: '#fff', backdropFilter: 'blur(50px)'}}>
-              <Cart cart={cart} products={props.products} removeFromCart={props.removeFromCart} addToCart={props.addToCart}/>
+            <Grid item xs={10}>
+              <TextField fullWidth label='city' variant='standard' value={city} onChange={e => setCity(e.target.value)}></TextField>
             </Grid>
-            </Grid> 
-        </div>
-
-    );
+            <Grid item xs={10}>
+              <TextField fullWidth label='state' variant='standard' value={state} onChange={e => setState(e.target.value)}></TextField>
+            </Grid>
+            <br></br>
+            <Typography variant='h5'>Payment</Typography>
+            <Box sx={{ borderColor: 'grey', borderRadius: 6, height: '2rem', marginBottom: '2rem', marginTop: '.5rem', bgcolor: 'white', padding: '1rem', alignItems: 'center' }}>
+              <CardElement id="card-element" iconStyle='solid' />
+            </Box>
+            <Button variant='contained' type='submit'>Pay</Button>
+          </form>
+        </Grid>
+        <Grid item sm={4} style={{ color: '#fff', backdropFilter: 'blur(50px)'}}>
+          <Cart cart={cart} products={props.products} removeFromCart={props.removeFromCart} addToCart={props.addToCart}/>
+        </Grid>
+      </Grid> 
+    </div>
+  );
 };
 
 export default Checkout;
